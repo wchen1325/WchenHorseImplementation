ba290b1b8b20cda430d03ddeff4d46151a19af8b
BT_1N3_02 <wchen1325@bths.edu>
2018-11-16 14:49:43 -0500

    ok done

---
+++ src/HorseBarn.java
@@ -0,0 +1,47 @@
+public class HorseBarn {
+    private Horse[] spaces;
+
+    public HorseBarn(Horse[] spaces){
+        this.spaces = spaces;
+    }
+
+
+    public int findHorseSpace(String name){
+        for(int i = 0 ; i < this.spaces.length; i++){
+            if( this .spaces[i]!= null && name.equals(this.spaces[i].getName()))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void consolidate() {
+        for(int i = 0; i < this.spaces.length-1; i++){
+            if (this.spaces[i] == null){
+                for (int j = i+1; j < this.spaces.length; j++){
+                    if (this.spaces[j] != null){
+                        this.spaces[i] = this.spaces[j];
+                        this.spaces[j] = null;
+                        j = this.spaces.length;
+                    }
+                }
+            }
+        }
+    }
+
+    public String toString(){
+        String barnList = "";
+
+        for(int i =0; i < this.spaces.length; i++){
+            if(this.spaces[i] != null) {
+                barnList = (barnList + this.spaces[i].getName()) + ", ";
+            }
+            else{
+                barnList = barnList + " null,";
+            }
+        }
+
+        return barnList;
+    }
+}
